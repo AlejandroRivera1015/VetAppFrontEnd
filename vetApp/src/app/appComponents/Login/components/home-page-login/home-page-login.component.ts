8f9be357c2f8266d62deeb20f4a5457bbcf700f2
AlexDev <ariverar151097@icloud.com>
2025-06-01 14:15:29 -0600

    add LoginForm component into home-page-loginComponent

--- vetApp/src/app/appComponents/Login/components/home-page-login/home-page-login.component.ts
+++ vetApp/src/app/appComponents/Login/components/home-page-login/home-page-login.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HomeSliderComponent } from '../home-slider/home-slider.component';
 import { WindowUtil } from '../../../../utils/WindowUtil';
 import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 @Component({
   selector: 'app-home-page-login',
-  imports: [CommonModule, HomeSliderComponent],
+  imports: [CommonModule, HomeSliderComponent,LoginFormComponent],
   standalone: true,
   templateUrl: './home-page-login.component.html',
   styleUrl: './home-page-login.component.css'
