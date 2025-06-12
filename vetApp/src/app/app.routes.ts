af62029032ead373f3d22594d08f5bdc37b39a0b
AlexDev <ariverar151097@icloud.com>
2025-06-12 10:41:56 -0600

    add userSchedule view into app routes

--- vetApp/src/app/app.routes.ts
+++ vetApp/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './security/auth.guard';
 
 export const routes: Routes = [
     {
         path: "",
         loadChildren: () => import('./appModules/log-in/log-in.module').then(m => m.LogInModule)
+    },
+    {
+        path: "user/schedule",
+        loadChildren: () => import('./appModules/user-schedule/user-schedule.module').then(m => m.UserScheduleModule),
+        canActivate:[AuthGuard],
+        data: {role  :'client'}
     }
 ];
