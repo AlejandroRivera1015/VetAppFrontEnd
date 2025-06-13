16de260244197f7458c82e1e7dc57bcf05e37916
AlexDev <ariverar151097@icloud.com>
2025-06-12 20:23:26 -0600

    add getters methods for YserDTO objects

--- vetApp/src/app/utils/DTO/DoctorDTO.ts
+++ vetApp/src/app/utils/DTO/DoctorDTO.ts
@@ -12,6 +12,27 @@ export class DoctorDTO {
 
 
 
+    public getId(): number {
+        return this.id;
+    }
+    public getName(): string {
+        return this.name;
+    }
+    public getRole(): string {
+        return this.role;
+    }
+    public getSpeciality(): string {
+        return this.speciality;
+    }
+    public getStartShiftAt(): Date {
+        return this.startShiftAt;
+    }
+    public getMedicalAppointments(): Array<Date> {
+        return this.medicalAppointments;
+    }
+
+
+
     constructor(id: number, name: string, role: string, speciality: string, startShiftAt: Date, medicalAppointments: Array<Date>){
         this.id = id;
         this.name = name;
