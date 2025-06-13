bf7ba95a48e14516222b6a7cecdd403c190775a2
AlexDev <ariverar151097@icloud.com>
2025-06-12 20:25:25 -0600

    update service to convert plain array of json into a DoctorDTO array on a behaviorSubject

--- vetApp/src/app/services/Appointments/appointments.service.ts
+++ vetApp/src/app/services/Appointments/appointments.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { DoctorDTO } from '../../utils/DTO/DoctorDTO';
 
 @Injectable({
@@ -8,15 +8,41 @@ import { DoctorDTO } from '../../utils/DTO/DoctorDTO';
 })
 export class AppointmentsService {
 
+
+  private availableDoctors: BehaviorSubject<Array<DoctorDTO>> = new BehaviorSubject<Array<DoctorDTO>>([]);
+
+
+  availableDoctorsRequest() : void {
+  this.httpClient.get<Array<any>>('http://localhost:8080/doctor/getAllAvailable',{
+      observe: 'body',
+      withCredentials: true
+    }).pipe(
+      map((availableDoctors : Array<any>) => {
+        return availableDoctors.map((doctor: any) => {
+          console.log(doctor);
+          return new DoctorDTO(
+            doctor.id,
+              doctor.name,
+              doctor.role,
+              doctor.speciality,
+              new Date(doctor.startShiftAt),
+              doctor.medicalAppointments.map((date: any) => new Date(date))
+            );
+      });
+      
+
+    })).subscribe((doctors: Array<DoctorDTO>) =>{
+      this.availableDoctors.next(doctors);
+    });
+  }
+
+getAvailableDoctors(): Observable<Array<DoctorDTO>>{
+  return this.availableDoctors.asObservable();
+}
+
+
   constructor(private httpClient: HttpClient) { }
 
 
-  getAvailableDoctors() : Observable<Array<DoctorDTO>> {
-    return this.httpClient.get<Array<DoctorDTO>>('http://localhost:8080/doctor/getAllAvailable',{
-      observe: 'body',
-      withCredentials: true
-    }).pipe(map((availableDoctors : Array<DoctorDTO>) => {
-      return availableDoctors;
-    }));
-  }
+
 }
