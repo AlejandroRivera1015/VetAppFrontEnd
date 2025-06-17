9c1e141844b659c073b8097345bd1c9d041620c5
AlexDev <ariverar151097@icloud.com>
2025-06-16 23:32:55 -0600

    global userSchedule component w/ appointments manage

--- vetApp/src/app/appComponents/UserSchedule/user-schedule-main-cont/user-schedule-main-cont.component.ts
+++ vetApp/src/app/appComponents/UserSchedule/user-schedule-main-cont/user-schedule-main-cont.component.ts
@@ -4,15 +4,19 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { AppointmentsService } from '../../../services/Appointments/appointments.service';
 import { DoctorDTO } from '../../../utils/DTO/DoctorDTO';
 import { DoctorDisplayComponent } from '../../DoctorsDisplay/doctor-display/doctor-display.component';
+import { AvailableHoursDisplayComponent } from '../../AvailableHoursDisplay/available-hours-display/available-hours-display.component';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Component({
   selector: 'app-user-schedule-main-cont',
-  imports: [HeaderComponent,DoctorDisplayComponent],
+  imports: [HeaderComponent,DoctorDisplayComponent,AvailableHoursDisplayComponent],
   standalone: true,
   templateUrl: './user-schedule-main-cont.component.html',
   styleUrl: './user-schedule-main-cont.component.css'
 })
 export class UserScheduleMainContComponent implements OnInit {
+
+  private appointments = new BehaviorSubject<Array<DoctorDTO>>([]);
 
   constructor(private appointMentsService: AppointmentsService){
 
@@ -20,7 +24,6 @@ export class UserScheduleMainContComponent implements OnInit {
 
   ngOnInit(): void {
     this.appointMentsService.availableDoctorsRequest();
-
   }
 
 
