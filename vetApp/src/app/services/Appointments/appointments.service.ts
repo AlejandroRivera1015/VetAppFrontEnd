cc4b1ae505cf019744ca356ca3476ba88903441c
AlexDev <ariverar151097@icloud.com>
2025-06-16 23:26:32 -0600

    update dr Service to handle doctorId and set dr schedule

--- vetApp/src/app/services/Appointments/appointments.service.ts
+++ vetApp/src/app/services/Appointments/appointments.service.ts
@@ -10,6 +10,8 @@ export class AppointmentsService {
 
 
   private availableDoctors: BehaviorSubject<Array<DoctorDTO>> = new BehaviorSubject<Array<DoctorDTO>>([]);
+  private selectedDoctor = new BehaviorSubject<number>(0);
+  private drSchedule = new BehaviorSubject<Array<Date>>([]);
 
 
   availableDoctorsRequest() : void {
@@ -27,18 +29,52 @@ export class AppointmentsService {
               new Date(doctor.startShiftAt),
               doctor.medicalAppointments.map((date: any) => new Date(date))
             );
-      });
-      
+      });      
 
     })).subscribe((doctors: Array<DoctorDTO>) =>{
       this.availableDoctors.next(doctors);
     });
   }
 
-getAvailableDoctors(): Observable<Array<DoctorDTO>>{
-  return this.availableDoctors.asObservable();
-}
+  getAvailableDoctors(): Observable<Array<DoctorDTO>>{
+    return this.availableDoctors.asObservable();
+  }
 
+  getSelectedDoctor(): Observable<number>{
+    return this.selectedDoctor.asObservable();
+  }
+
+  getDrSchedule():Observable<Array<Date>>{
+    return this.drSchedule.asObservable();
+  }
+
+  setSelectedDoctor(doctorId: number):void{
+    this.selectedDoctor.next(doctorId);
+  }
+
+  getDoctorAvailableHours(doctorId: number):void{   
+    
+    let drAvailability:Array<Date> = [];
+
+    this.getAvailableDoctors().pipe(
+      map((availableDoctors : Array<DoctorDTO>)=>{
+
+        let selectedDr  = availableDoctors.find(doctor => doctor.getId() === doctorId);
+
+        if(selectedDr){
+          let doctorStartShift: Date = selectedDr.getStartShiftAt();
+          let doctorMedicalAppointments : Array<Date> = selectedDr.getMedicalAppointments();
+          drAvailability = DoctorDTO.setDoctorAvailableHours(doctorStartShift,doctorMedicalAppointments);
+          return drAvailability;
+        }
+        else
+          return [];
+        })
+      ).subscribe((drAvailability : Array<Date>) =>{
+        this.drSchedule.next(drAvailability);
+      });
+
+  }
 
   constructor(private httpClient: HttpClient) { }
 
