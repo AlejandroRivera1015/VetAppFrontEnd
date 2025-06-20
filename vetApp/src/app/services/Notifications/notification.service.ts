acbcaf645240cfd0f2b3ab8d778b8827c87aad17
AlexDev <ariverar151097@icloud.com>
2025-06-19 22:40:58 -0600

    service notification message handle

--- vetApp/src/app/services/Notifications/notification.service.ts
+++ vetApp/src/app/services/Notifications/notification.service.ts
@@ -8,6 +8,8 @@ export class NotificationService {
 
 
   private isOnScreen = new BehaviorSubject<boolean>(true);
+  private confirmAction = new BehaviorSubject<boolean>(false);
+  public mainMessage = new BehaviorSubject<string>('');
 
   constructor() { }
 
@@ -29,5 +31,23 @@ export class NotificationService {
 
   }
 
+
+  public setMainMessage(message: string):void{
+    
+    this.mainMessage.next(message);
+  }
+
+  public getMessage(): Observable<string>{
+    return this.mainMessage.asObservable();
+  }
+
+
+  public setConfirmation(userAuthorization: boolean):void{
+    this.confirmAction.next(userAuthorization);
+  }
+
+  public getConfirmation():Observable<boolean>{
+    return this.confirmAction.asObservable();
+  }
   
 }
